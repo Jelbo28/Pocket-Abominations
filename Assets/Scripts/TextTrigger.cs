5791a2612162409313fb5d61cd92324791dc2283
Aram Joseph <ajoseph@mr.preble.com>
2016-04-28 08:19:59 -0400

    First Update

---
+++ Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTrigger : MonoBehaviour 
+{
+	void OnStart()
+	{
+		gameObject.GetComponent<MeshRenderer>().enabled = false;
+	}
+
+	void OntriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			gameObject.GetComponent<MeshRenderer>().enabled = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		gameObject.GetComponent<MeshRenderer>().enabled = false;
+	}
+}
