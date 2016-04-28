88c71d10c376fbd1f5b72810b32e26aa983ad077
Aram Joseph <ajoseph@mr.preble.com>
2016-04-28 10:22:50 -0400

    Update

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	public void Play()
+	{
+		Application.LoadLevel ("Introduction");
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+
+	public void Credits()
+	{
+		Application.LoadLevel ("Credits");
+	}
+}
---
+++ Assets/Scripts/LerpRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("Rotate", false);
+            //gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, new Quaternion(0, 180, 0, 0), 0.2f);
+        }
+	
+	}
+}
