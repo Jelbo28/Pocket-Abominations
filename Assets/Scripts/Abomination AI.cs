d40d63e48589d4608bf14d0cf9b8bc2303a7e03d
Aram Joseph <ajoseph@mr.preble.com>
2016-05-02 10:25:39 -0400

    Newest Stuff

---
+++ Assets/Scripts/Abomination AI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbominationAI : MonoBehaviour
+{
+    bool gotRandom;
+    float storeRandom;
+
+    void Update()
+    {
+        Walk();
+    }
+
+
+    void Walk()
+    {
+        if (!gotRandom)
+        {
+            storeRandom = Random.Range(1, 6);
+            gotRandom = true;
+        }
+    }
+}
