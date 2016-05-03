efb86606e90c5c55498148021145f8f80a9f1066
Aram Joseph <ajoseph@mr.preble.com>
2016-05-03 10:22:57 -0400

    Credits

---
+++ Assets/Scripts/AbominationAI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbominationAI : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10f;
+
+    Vector3 wayPoint;
+
+    float timeRandom = 0f;
+
+    void Start()
+    {
+        Wander();
+    }
+
+    void Update()
+    {
+        transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
+        if((transform.position - wayPoint).magnitude < 3)
+        {
+            StartCoroutine(Wait());
+            Wander();
+        }
+    }
+
+    void Wander()
+    {
+        timeRandom = Random.Range(1f, 6f);
+        wayPoint = Random.insideUnitSphere * 47;
+        wayPoint.y = 1;
+        transform.LookAt(wayPoint);
+        Debug.Log(wayPoint + " and " + (transform.position - wayPoint).magnitude);
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(timeRandom);
+        Debug.Log("Wait " + timeRandom);
+    }
+}
---
+++ Assets/Scripts/AnimationTransitions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AnimationTransitions : MonoBehaviour
+{
+    [SerializeField]
+    Animator exitAnimator;
+
+    public void Menu()
+    {
+        Debug.Log("Back to menu.");
+        SceneManager.LoadScene("openingScene");
+        // Use this to load whatever scene the title screen is on.
+        //SceneManager.LoadScene(1);
+    }
+
+    public void ButtonGlow()
+    {
+        exitAnimator.SetBool("ButtonGlow", true);
+    }
+}
