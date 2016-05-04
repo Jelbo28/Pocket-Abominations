034a92380b07d94a4d91eb54ff3fa2ed9887d933
Aram Joseph <ajoseph@mr.preble.com>
2016-05-04 10:21:41 -0400

    Stuff

--- Assets/Scripts/AbominationAI.cs
+++ Assets/Scripts/AbominationAI.cs
@@ -8,10 +8,13 @@ public class AbominationAI : MonoBehaviour
 
     Vector3 wayPoint;
 
+    bool wait;
+
     float timeRandom = 0f;
 
     void Start()
     {
+        wait = false;
         Wander();
     }
 
@@ -20,23 +23,28 @@ public class AbominationAI : MonoBehaviour
         transform.position += transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime;
         if((transform.position - wayPoint).magnitude < 3)
         {
-            StartCoroutine(Wait());
+            //StartCoroutine(Wait());
             Wander();
         }
     }
 
     void Wander()
     {
-        timeRandom = Random.Range(1f, 6f);
-        wayPoint = Random.insideUnitSphere * 47;
-        wayPoint.y = 1;
-        transform.LookAt(wayPoint);
-        Debug.Log(wayPoint + " and " + (transform.position - wayPoint).magnitude);
+        if(wait == false)
+        {
+            timeRandom = Random.Range(1f, 6f);
+            wayPoint = Random.insideUnitSphere * 47;
+            wayPoint.y = 1;
+            transform.LookAt(wayPoint);
+            Debug.Log(wayPoint + " and " + (transform.position - wayPoint).magnitude);
+        }
+        wait = true;
     }
 
     IEnumerator Wait()
     {
         yield return new WaitForSeconds(timeRandom);
+        wait = false;
         Debug.Log("Wait " + timeRandom);
     }
 }
