9a63cbb31a6f9fe7d5b8108de18c687d38898572
Aram Joseph <ajoseph@mr.preble.com>
2016-05-05 08:37:39 -0400

    Stuff

---
+++ Assets/Scripts/PodiumText.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PodiumText : MonoBehaviour 
+{
+	[SerializeField]
+	GameObject hello;
+	[SerializeField] 
+	GameObject Next;
+
+	bool triggerActive = false;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		hello.SetActive(true);
+		triggerActive = true;
+
+	}
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.E) && triggerActive == true)
+		{
+			hello.SetActive (false);
+			Next.SetActive (true);
+		}
+	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			hello.SetActive(false);
+			Next.SetActive (false);
+		}
+	}
+}
