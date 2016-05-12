708c00115b7183908b01e239467bbdd6ebbb58dc
Aram Joseph <ajoseph@mr.preble.com>
2016-05-12 09:32:51 -0400

    Stuff

--- Assets/Scripts/WalkAround.cs
+++ Assets/Scripts/WalkAround.cs
@@ -18,7 +18,7 @@ public class WalkAround : MonoBehaviour {
 	private Transform myTransform;
 	private Transform stopDistance;
 
-	public List <Vector3> wayPoints;
+	public List <GameObject> GO_wayPoints;
 
 	Vector3 nextWayPoint;
 
@@ -37,7 +37,7 @@ public class WalkAround : MonoBehaviour {
 	{
 		Debug.DrawLine (target.position, myTransform.position, Color.green);
 
-		myTransform.rotation = Quaternion.Slerp (myTransform.rotation, Quaternion.LookRotation (target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+		myTransform.rotation = Quaternion.Slerp (myTransform.rotation, Quaternion.LookRotation (target.position - myTransform.position), rotationSpeed * Time.deltaTime * 2);
 
 		myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
 
@@ -48,87 +48,61 @@ public class WalkAround : MonoBehaviour {
 	{
 		if (collider.gameObject.tag == "WayPoint0") 
 		{
-			//nextWayPoint = wayPoints[1];
-
 			StartCoroutine (WaitToWalk (0));
-
 		}
 		if(collider.gameObject.tag == "WayPoint1")
 		{
-			//nextWayPoint = wayPoints [2];
-			//StartCoroutine (WaitToWalk ());
 			StartCoroutine (WaitToWalk (1));
 		}
 		if(collider.gameObject.tag == "WayPoint2")
 		{
-			//nextWayPoint = wayPoints [3];
 			StartCoroutine (WaitToWalk (2));
-			//target.position = wayPoints [3];
 		}
 		if(collider.gameObject.tag == "WayPoint3")
 		{
-			//nextWayPoint = wayPoints [4];
 			StartCoroutine (WaitToWalk (3));
-			//target.position = wayPoints [4];
 		}
 		if(collider.gameObject.tag == "WayPoint4")
 		{
-			//nextWayPoint = wayPoints [5];
 			StartCoroutine (WaitToWalk (4));
-			//target.position = wayPoints [5];
 		}
 		if(collider.gameObject.tag == "WayPoint5")
 		{
-			//nextWayPoint = wayPoints [6];
 			StartCoroutine (WaitToWalk (5));
-			//target.position = wayPoints [6];
 		}
 		if(collider.gameObject.tag == "WayPoint6")
 		{
-			//nextWayPoint = wayPoints [7];
 			StartCoroutine (WaitToWalk (6));
-			//target.position = wayPoints [7];
 		}
 		if(collider.gameObject.tag == "WayPoint7")
 		{
-			//nextWayPoint = wayPoints [8];
 			StartCoroutine (WaitToWalk (7));
-			//target.position = wayPoints [8];
 		}
 		if(collider.gameObject.tag == "WayPoint8")
 		{
-			//nextWayPoint = wayPoints [9];
 			StartCoroutine (WaitToWalk (8));
-			//target.position = wayPoints [9];
 		}
 		if(collider.gameObject.tag == "WayPoint9")
 		{
-			//nextWayPoint = wayPoints [10];
 			StartCoroutine (WaitToWalk (9));
-			//target.position = wayPoints [10];
 		}
 		if(collider.gameObject.tag == "WayPoint10")
 		{
-			//nextWayPoint = wayPoints [11];
 			StartCoroutine (WaitToWalk (10));
-			//target.position = wayPoints [11];
 		}
 		if (collider.gameObject.tag == "WayPoint11") 
 		{
-			//nextWayPoint = wayPoints[0]
-			StartCoroutine (WaitToWalk (-1));
-			//target.position = wayPoints [0];
+			StartCoroutine (WaitToWalk (11));
 		}
-
-
-
-
 
 	}
 
 	IEnumerator WaitToWalk(int previousway)
 	{
 		yield return new WaitForSeconds (1);
-		target.position = wayPoints [previousway + 1];
+		GO_wayPoints [previousway].SetActive (false);
+		GO_wayPoints [(previousway + 1) % 12].SetActive (true);
+		target.position = GO_wayPoints [(previousway + 1) % 12].transform.position;
+
 	}
 }
