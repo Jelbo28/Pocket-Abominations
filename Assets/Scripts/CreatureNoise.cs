8fe5b91a47d0e6a73c0ab546dfc0d21a92375b8e
Aram Joseph <ajoseph@utc4me.org>
2016-05-13 07:00:24 -0400

    Thangs

---
+++ Assets/Scripts/CreatureNoise.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CreatureNoise : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pressE;
+    [SerializeField]
+    Text text;
+
+    bool triggerActive = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            text.text = (" : Pet");
+            pressE.SetActive(true);
+            triggerActive = true;
+        }
+    }
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && triggerActive == true)
+        {
+            pressE.SetActive(false);
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+	}
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            pressE.SetActive(false);
+        }
+    }
+}
